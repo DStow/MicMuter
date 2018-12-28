f0233a4253a6c31018495773265211f935385a67
Dan Stow <Dan_Stow@live.co.uk>
2018-12-28 14:20:25 +0000

    Set up keyboard loop and update manager to get microphone not speaker

--- MicMuter/MainWindow.xaml.cs
+++ MicMuter/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +22,44 @@ namespace MicMuter
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool _running = true;
+        private bool _keysDown = false;
         public MainWindow()
         {
             InitializeComponent();
+
+            // Create thread to listen for keyboard
+            Thread thread = new Thread(() =>
+            {
+                while (_running)
+                {
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        if (Keyboard.IsKeyDown(Key.M) && Keyboard.IsKeyDown(Key.LeftShift) && _keysDown == false)
+                        {
+                            // Toggle Mute
+                            Console.WriteLine("Keys Down!");
+                            AudioManager.ToggleMasterVolumeMute();
+                            _keysDown = true;
+                        }
+                        else if ((Keyboard.IsKeyUp(Key.M) || Keyboard.IsKeyUp(Key.LeftShift)) && _keysDown == true)
+                        {
+                            Console.WriteLine("Keys Up!");
+                            _keysDown = false;
+                        }
+                    });
+
+                    Thread.Sleep(100);
+                }
+            });
+
+            thread.Start();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            _running = false;
+            base.OnClosing(e);
         }
     }
 }
